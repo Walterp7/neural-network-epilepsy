190ef4e5ef9e05bc6805911361b44525e8a19701
beata.strack@gmail.com <beata.strack@gmail.com@de725449-041e-4d08-be5f-578b637f8627>
2011-03-16 18:46:37 +0000

    Degrees Analysis

--- trunk/NeuralNetwork/src/simulationPackage/AnalyseNetwork.java
+++ trunk/NeuralNetwork/src/simulationPackage/AnalyseNetwork.java
@@ -7,59 +7,89 @@ import java.util.Map.Entry;
 import networkPackage.Network;
 import neuronPackage.NetworkNode;
 import neuronPackage.Neuron;
+import neuronPackage.Synapse;
 import neuronPackage.Type;
 
 class AnalyseNetwork {
 
 	void getDegrees(Network n) {
 		List<NetworkNode> l = n.getAllNodes();
+
+		HashMap<NetworkNode, Integer> allNodesDeg = new HashMap<NetworkNode, Integer>();
+
+		for (NetworkNode node : l) {
+			if (node instanceof Neuron) {
+				if (allNodesDeg.containsKey(node)) {
+					int deg = allNodesDeg.get(node)
+							+ ((Neuron) node).getNeuronConnections().size();
+					allNodesDeg.remove(node);
+					allNodesDeg.put(node, deg);
+				} else {
+					int deg = ((Neuron) node).getNeuronConnections().size();
+					allNodesDeg.put(node, deg);
+				}
+			}
+			if (node instanceof Synapse) {
+				Neuron neur = ((Synapse) node).getPostSynapticNeuron();
+				if (allNodesDeg.containsKey(neur)) {
+					int deg = allNodesDeg.get(neur) + 1;
+					allNodesDeg.remove(neur);
+					allNodesDeg.put(neur, deg);
+				} else {
+
+					allNodesDeg.put(neur, 1);
+				}
+			}
+		}
+
 		HashMap<Integer, Integer> rsmap = new HashMap<Integer, Integer>();
 		HashMap<Integer, Integer> ltsmap = new HashMap<Integer, Integer>();
 		HashMap<Integer, Integer> fsmap = new HashMap<Integer, Integer>();
 		HashMap<Integer, Integer> ibmap = new HashMap<Integer, Integer>();
-		for (NetworkNode node : l) {
-			if (node instanceof Neuron) {
-				int degree = ((Neuron) node).getNeuronConnections().size();
-				if (((Neuron) node).getType() == Type.RS) {
-					if (rsmap.containsKey(degree)) {
-						int number = rsmap.get(degree) + 1;
-						rsmap.remove(degree);
-						rsmap.put(degree, number);
-					} else {
-						rsmap.put(degree, 1);
-					}
-				}
-				if (((Neuron) node).getType() == Type.FS) {
-					if (fsmap.containsKey(degree)) {
-						int number = fsmap.get(degree) + 1;
-						fsmap.remove(degree);
-						fsmap.put(degree, number);
-					} else {
-						fsmap.put(degree, 1);
-					}
-				}
 
-				if (((Neuron) node).getType() == Type.LTS) {
-					if (ltsmap.containsKey(degree)) {
-						int number = ltsmap.get(degree) + 1;
-						ltsmap.remove(degree);
-						ltsmap.put(degree, number);
-					} else {
-						ltsmap.put(degree, 1);
-					}
+		for (Entry<NetworkNode, Integer> entry : allNodesDeg.entrySet()) {
+			Neuron neuron = (Neuron) entry.getKey();
+			int degree = entry.getValue();
+			if (neuron.getType() == Type.RS) {
+				if (rsmap.containsKey(degree)) {
+					int number = rsmap.get(degree) + 1;
+					rsmap.remove(degree);
+					rsmap.put(degree, number);
+				} else {
+					rsmap.put(degree, 1);
 				}
+			}
+			if (neuron.getType() == Type.FS) {
+				if (fsmap.containsKey(degree)) {
+					int number = fsmap.get(degree) + 1;
+					fsmap.remove(degree);
+					fsmap.put(degree, number);
+				} else {
+					fsmap.put(degree, 1);
+				}
+			}
 
-				if (((Neuron) node).getType() == Type.IB) {
-					if (ibmap.containsKey(degree)) {
-						int number = ibmap.get(degree) + 1;
-						ibmap.remove(degree);
-						ibmap.put(degree, number);
-					} else {
-						ibmap.put(degree, 1);
-					}
+			if (neuron.getType() == Type.LTS) {
+				if (ltsmap.containsKey(degree)) {
+					int number = ltsmap.get(degree) + 1;
+					ltsmap.remove(degree);
+					ltsmap.put(degree, number);
+				} else {
+					ltsmap.put(degree, 1);
+				}
+			}
+
+			if (neuron.getType() == Type.IB) {
+				if (ibmap.containsKey(degree)) {
+					int number = ibmap.get(degree) + 1;
+					ibmap.remove(degree);
+					ibmap.put(degree, number);
+				} else {
+					ibmap.put(degree, 1);
 				}
 			}
 		}
+
 		System.out.println("for RS neurons:");
 		for (Entry<Integer, Integer> entry : rsmap.entrySet()) {
 			System.out.println(entry.getKey() + " " + entry.getValue());
