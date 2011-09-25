f997f7ea2ac68b48e8671feaf5f24c2d94e4cd74
beata.strack <beata.strack@gmail.com>
2011-09-25 21:33:15 +0000

    plotting like for proposal

--- NeuralNetwork/src/networkGUI/LabelAxis.java
+++ NeuralNetwork/src/networkGUI/LabelAxis.java
@@ -9,16 +9,18 @@ import java.util.List;
 import org.jfree.chart.axis.NumberTick;
 import org.jfree.chart.axis.SymbolAxis;
 import org.jfree.chart.axis.Tick;
-import org.jfree.chart.axis.ValueAxis;
 import org.jfree.text.TextUtilities;
 import org.jfree.ui.RectangleEdge;
 import org.jfree.ui.TextAnchor;
 
 public class LabelAxis extends SymbolAxis {
+	String[] labels;
+	int[] labelIndexes;
 
-	public LabelAxis(String label, String[] sv) {
+	public LabelAxis(String label, String[] sv, String[] labs, int[] labIndx) {
 		super(label, sv);
-		// TODO Auto-generated constructor stub
+		labels = labs;
+		labelIndexes = labIndx;
 	}
 
 	private static final long serialVersionUID = 1L;
@@ -34,169 +36,76 @@ public class LabelAxis extends SymbolAxis {
 		g2.setFont(tickLabelFont);
 
 		double size = getTickUnit().getSize();
-		int count = calculateVisibleTickCount();
+
 		double lowestTickValue = calculateLowestVisibleTickValue();
 
 		double previousDrawnTickLabelPos = 0.0;
 		double previousDrawnTickLabelLength = 0.0;
 
-		if (count <= ValueAxis.MAXIMUM_TICK_COUNT) {
-			for (int i = 0; i < count; i++) {
-				double currentTickValue = lowestTickValue + (i * size);
-				double yy = valueToJava2D(currentTickValue, dataArea, edge);
-				String tickLabel;
-				NumberFormat formatter = getNumberFormatOverride();
-				if (formatter != null) {
-					tickLabel = formatter.format(currentTickValue);
-				}
-				else {
-					tickLabel = valueToString(currentTickValue);
-				}
-
-				// avoid to draw overlapping tick labels
-				// Rectangle2D bounds = TextUtilities.getTextBounds(tickLabel,
-				// g2,
-				// g2.getFontMetrics());
-				// double tickLabelLength = isVerticalTickLabels()
-				// ? bounds.getWidth() : bounds.getHeight();
-				// boolean tickLabelsOverlapping = false;
-				// if (i > 0) {
-				// double avgTickLabelLength = (previousDrawnTickLabelLength
-				// + tickLabelLength) / 2.0;
-				// if (Math.abs(yy - previousDrawnTickLabelPos)
-				// < avgTickLabelLength) {
-				// tickLabelsOverlapping = true;
-				// }
-				// }
-				// if (tickLabelsOverlapping && tickLabel == "") {
-				// tickLabel = ""; // don't draw this tick label
-				// }
-				// else {
-				// // remember these values for next comparison
-				// previousDrawnTickLabelPos = yy;
-				// previousDrawnTickLabelLength = tickLabelLength;
-				// }
-
-				TextAnchor anchor = null;
-				TextAnchor rotationAnchor = null;
-				double angle = 0.0;
-				if (isVerticalTickLabels()) {
-					anchor = TextAnchor.BOTTOM_CENTER;
-					rotationAnchor = TextAnchor.BOTTOM_CENTER;
-					if (edge == RectangleEdge.LEFT) {
-						angle = -Math.PI / 2.0;
-					}
-					else {
-						angle = Math.PI / 2.0;
-					}
-				}
-				else {
-					if (edge == RectangleEdge.LEFT) {
-						anchor = TextAnchor.CENTER_RIGHT;
-						rotationAnchor = TextAnchor.CENTER_RIGHT;
-					}
-					else {
-						anchor = TextAnchor.CENTER_LEFT;
-						rotationAnchor = TextAnchor.CENTER_LEFT;
-					}
-				}
-				Tick tick;
-				if (tickLabel == "") {
-					tick = null;
-				} else {
-					tick = new NumberTick(new Double(currentTickValue),
-							tickLabel, anchor, rotationAnchor, angle);
-				}
-				ticks.add(tick);
+		for (int i = 0; i < labels.length; i++) {
+			double currentTickValue = lowestTickValue + (labelIndexes[i] * size);
+			double yy = valueToJava2D(currentTickValue, dataArea, edge);
+			String tickLabel;
+			NumberFormat formatter = getNumberFormatOverride();
+			if (formatter != null) {
+				tickLabel = formatter.format(currentTickValue);
 			}
-		}
-		return ticks;
+			else {
+				tickLabel = valueToString(currentTickValue);
+			}
+			tickLabel = labels[i];
 
-	}
+			// avoid to draw overlapping tick labels
+			Rectangle2D bounds = TextUtilities.getTextBounds(tickLabel, g2,
+					g2.getFontMetrics());
+			double tickLabelLength = isVerticalTickLabels()
+					? bounds.getWidth() : bounds.getHeight();
+			boolean tickLabelsOverlapping = false;
+			if (i > 0) {
+				double avgTickLabelLength = (previousDrawnTickLabelLength
+						+ tickLabelLength) / 2.0;
+				if (Math.abs(yy - previousDrawnTickLabelPos)
+				< avgTickLabelLength) {
+					tickLabelsOverlapping = true;
+				}
+			}
+			if (tickLabelsOverlapping) {
+				tickLabel = ""; // don't draw this tick label
+			}
+			else {
+				// remember these values for next comparison
+				previousDrawnTickLabelPos = yy;
+				previousDrawnTickLabelLength = tickLabelLength;
+			}
 
-	@Override
-	protected List refreshTicksHorizontal(Graphics2D g2,
-			Rectangle2D dataArea,
-			RectangleEdge edge) {
-
-		List ticks = new java.util.ArrayList();
-
-		Font tickLabelFont = getTickLabelFont();
-		g2.setFont(tickLabelFont);
-
-		double size = getTickUnit().getSize();
-		int count = calculateVisibleTickCount();
-		double lowestTickValue = calculateLowestVisibleTickValue();
-
-		double previousDrawnTickLabelPos = 0.0;
-		double previousDrawnTickLabelLength = 0.0;
-
-		if (count <= ValueAxis.MAXIMUM_TICK_COUNT) {
-			for (int i = 0; i < count; i++) {
-				double currentTickValue = lowestTickValue + (i * size);
-				double xx = valueToJava2D(currentTickValue, dataArea, edge);
-				String tickLabel;
-				NumberFormat formatter = getNumberFormatOverride();
-				if (formatter != null) {
-					tickLabel = formatter.format(currentTickValue);
+			TextAnchor anchor = null;
+			TextAnchor rotationAnchor = null;
+			double angle = 0.0;
+			if (isVerticalTickLabels()) {
+				anchor = TextAnchor.BOTTOM_CENTER;
+				rotationAnchor = TextAnchor.BOTTOM_CENTER;
+				if (edge == RectangleEdge.LEFT) {
+					angle = -Math.PI / 2.0;
 				}
 				else {
-					tickLabel = valueToString(currentTickValue);
+					angle = Math.PI / 2.0;
 				}
-
-				// avoid to draw overlapping tick labels
-				Rectangle2D bounds = TextUtilities.getTextBounds(tickLabel, g2,
-						g2.getFontMetrics());
-				double tickLabelLength = isVerticalTickLabels()
-						? bounds.getHeight() : bounds.getWidth();
-				boolean tickLabelsOverlapping = false;
-				if (i > 0) {
-					double avgTickLabelLength = (previousDrawnTickLabelLength
-							+ tickLabelLength) / 2.0;
-					if (Math.abs(xx - previousDrawnTickLabelPos)
-					< avgTickLabelLength) {
-						tickLabelsOverlapping = true;
-					}
-				}
-				if (tickLabelsOverlapping && tickLabel == "") {
-					tickLabel = ""; // don't draw this tick label
-				}
-				else {
-					// remember these values for next comparison
-					previousDrawnTickLabelPos = xx;
-					previousDrawnTickLabelLength = tickLabelLength;
-				}
-
-				TextAnchor anchor = null;
-				TextAnchor rotationAnchor = null;
-				double angle = 0.0;
-				if (isVerticalTickLabels()) {
+			}
+			else {
+				if (edge == RectangleEdge.LEFT) {
 					anchor = TextAnchor.CENTER_RIGHT;
 					rotationAnchor = TextAnchor.CENTER_RIGHT;
-					if (edge == RectangleEdge.TOP) {
-						angle = Math.PI / 2.0;
-					}
-					else {
-						angle = -Math.PI / 2.0;
-					}
 				}
 				else {
-					if (edge == RectangleEdge.TOP) {
-						anchor = TextAnchor.BOTTOM_CENTER;
-						rotationAnchor = TextAnchor.BOTTOM_CENTER;
-					}
-					else {
-						anchor = TextAnchor.TOP_CENTER;
-						rotationAnchor = TextAnchor.TOP_CENTER;
-					}
+					anchor = TextAnchor.CENTER_LEFT;
+					rotationAnchor = TextAnchor.CENTER_LEFT;
 				}
-				Tick tick = new NumberTick(new Double(currentTickValue),
-						tickLabel, anchor, rotationAnchor, angle);
-				ticks.add(tick);
 			}
+			Tick tick = new NumberTick(new Double(currentTickValue),
+					tickLabel, anchor, rotationAnchor, angle);
+			ticks.add(tick);
 		}
 		return ticks;
 
 	}
-
 }
