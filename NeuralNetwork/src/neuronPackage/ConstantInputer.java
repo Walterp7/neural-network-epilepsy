323c7162c2d47fead614a81df2c3c285c78b8128
beata.strack <beata.strack@gmail.com>
2011-12-06 18:11:07 +0000

    constant inputer

---
+++ NeuralNetwork/src/neuronPackage/ConstantInputer.java
@@ -0,0 +1,47 @@
+package neuronPackage;
+
+import java.util.ArrayList;
+
+import networkPackage.Network;
+
+public class ConstantInputer extends Inputer {
+	private final double value;
+
+	protected final ArrayList<Neuron> inputConnections = new ArrayList<Neuron>();
+
+	@Override
+	public void addConnection(Neuron n, Network net) {
+		// inputConnections.add(n);
+
+		inputConnections.add(n);
+
+	}
+
+	public ConstantInputer(double v) {
+		value = v;
+
+	}
+
+	@Override
+	public Status advance(double timeStep, double timeofSimulation) {
+
+		for (Neuron n : inputConnections) {
+			n.addInput(value, timeStep, timeofSimulation);
+
+		}
+
+		return null;
+	}
+
+	@Override
+	public void setCurrentInput() {
+
+	}
+
+	@Override
+	public void addInput(double val, double time, double timeStep) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
