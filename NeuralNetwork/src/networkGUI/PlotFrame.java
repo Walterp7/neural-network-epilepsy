ad38319a06ffdda0d4902e154474d0da66443762
beata.strack <beata.strack@gmail.com>
2012-03-19 18:02:05 +0000

    Saving on one button

---
+++ NeuralNetwork/src/networkGUI/PlotFrame.java
@@ -0,0 +1,21 @@
+package networkGUI;
+
+import java.io.IOException;
+
+import javax.swing.JFrame;
+
+public abstract class PlotFrame extends JFrame {
+
+	public PlotFrame(String title) {
+
+		super(title);
+	}
+
+	public PlotFrame() {
+
+	}
+
+	public void save(String pathName) throws IOException {
+
+	}
+}
